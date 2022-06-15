f1ed6d5cfd6247d5f41ccf1bd06d409a10d53ae2
James Alhassan <jamesalhassan052@gmail.com>
2022-06-15 02:49:35 +0100

    value entered
    if value not empty/truthy  and editflag being true

--- app.js
+++ app.js
@@ -18,11 +18,52 @@ function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if (value !== "" && editFlag === false) {
-  } else if (value !== "" && editFlag === true) {
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    // add class to element
+    element.classList.add("grocery-item");
+
+    // add attribute and it's value (id)
+    const atrribute = document.createAttribute("data-id");
+    atrribute.value = id;
+
+    //  append atrribute to element
+
+    element.setAttributeNode(atrribute);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+
+    // append child (element-article) to the div wit class of grocery-list
+    list.appendChild(element);
+    // display alert
+    displayAlert(`${value} added to the lst`, "success");
+
+    // show container( div with class of grocery-container) which has been hidden in css
+    container.classList.add("show-container");
+  } else if (value && editFlag) {
   } else {
+    displayAlert("please enter value", "danger");
   }
 }
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+}
+
+// remove alert
+setTimeout(function () {
+  alert.textContent = "";
+  alert.classList.remove(`alert-${action}`);
+}, 3000);
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
